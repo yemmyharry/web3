3e6a8c415bf7331e87c57c476b343ddbeb778b5f
yemmyharry <yemmyharry@gmail.com>
2023-06-06 14:29:24 +0100

    fix calculation logic

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ function calculateBMI() {
     // Get the user's height and weight
     let height = document.getElementById("height").value;
     let weight = document.getElementById("weight").value;
+
+    //  check if the user input is a number and not empty
+    if (isNaN(height) || isNaN(weight) || height == "" || weight == "") {
+        document.getElementById("result").innerHTML = "Please input a number.";
+        return;
+    } else if (height <= 0 || weight <= 0) {
+        document.getElementById("result").innerHTML = "Please input a number greater than 0.";
+        return;
+    }
   
     // Calculate the BMI
     let bmi = weight / (height * height);
